b379b9613596d6f6ddd6e8d188ba9424669523e9
Fatlum <fatlum@itsfatlum.de>
2026-02-02 13:05:06 +0100

    Replaced hass.logger with _LOGGER to fix setup error.

--- custom_components/nintendo_switch_status/coordinator.py
+++ custom_components/nintendo_switch_status/coordinator.py
@@ -1,15 +1,18 @@
 from datetime import timedelta
+import logging
 import aiohttp
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 from homeassistant.core import HomeAssistant
 from .const import SCAN_INTERVAL
+
+_LOGGER = logging.getLogger(__name__)
 
 class NintendoSwitchCoordinator(DataUpdateCoordinator):
     def __init__(self, hass: HomeAssistant, api_url: str):
         self.api_url = api_url
         super().__init__(
             hass,
-            logger=hass.logger,
+            _LOGGER,
             name="nintendo_switch_status",
             update_interval=timedelta(seconds=SCAN_INTERVAL),
         )
@@ -20,6 +23,6 @@ class NintendoSwitchCoordinator(DataUpdateCoordinator):
                 async with session.get(self.api_url, timeout=10) as resp:
                     resp.raise_for_status()
                     return await resp.json()
-        except Exception:
-            # Let the coordinator handle the retry logic and log the exception
+        except Exception as err:
+            _LOGGER.exception("Error fetching Nintendo Switch presence data")
             raise
